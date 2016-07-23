8b2ba7db5cfd95b77c9d94f320e82128a7ebddd9
Omair <omair@gems.techverx.com>
2016-07-23 23:17:00 +0500

    saving changes

--- db/migrate/20160723084247_create_market_studies.rb
+++ db/migrate/20160723084247_create_market_studies.rb
@@ -3,7 +3,6 @@ class CreateMarketStudies < ActiveRecord::Migration
     create_table :market_studies do |t|
       t.string :title
       t.text :content
-
       t.timestamps null: false
     end
   end
