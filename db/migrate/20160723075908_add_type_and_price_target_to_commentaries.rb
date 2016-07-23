151d5fbdb22a3203693e759ee3a3dd334da3e79d
Omair <omair@gems.techverx.com>
2016-07-23 18:17:38 +0500

    saving chagnes

--- db/migrate/20160723075908_add_type_and_price_target_to_commentaries.rb
+++ db/migrate/20160723075908_add_type_and_price_target_to_commentaries.rb
@@ -1,7 +1,6 @@
 class AddTypeAndPriceTargetToCommentaries < ActiveRecord::Migration
   def change
     add_column :commentaries, :typee, :string
-    remove_column :commentaries, :type
     add_column :commentaries, :priceTarget, :string
   end
 end
